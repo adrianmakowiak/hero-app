f100029641a4d2a5548256a78bda8f1880e380e7
AdisMak <adrian.makowiak@gmail.com>
2021-01-30 22:05:25 +0100

    add fake api to hero app

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 /* eslint-disable no-case-declarations */
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import React, { useReducer } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
   Route,
   Link,
 } from 'react-router-dom';
+import Loader from 'react-loader-spinner';
 
 import './App.css';
 import Dashboard from './components/Dashboard';
@@ -16,9 +17,13 @@ import { HEROES } from './mocks';
 import { IHero } from './models';
 
 const initState: InitState = {
-  heroes: HEROES,
+  heroes: [],
   selectedHeroId: null,
 };
+
+const delay = (millis: number) => new Promise<void>((resolve, reject) => {
+  setTimeout((_) => resolve(), millis);
+});
 
 export const StateContext = React.createContext<InitState>(initState);
 export const DispatchContext = React.createContext<React.Dispatch<IAction>>(() => {});
@@ -35,6 +40,11 @@ type IAction = {
 
 const heroesReducer = (state: InitState, action: IAction): InitState => {
   switch (action.type) {
+    case 'SET_HEROES':
+      return {
+        ...state,
+        heroes: action.payload,
+      };
     case 'CHANGE_HERO':
       return {
         ...state,
@@ -62,6 +72,18 @@ const heroesReducer = (state: InitState, action: IAction): InitState => {
 
 function App() {
   const [state, dispatch] = useReducer(heroesReducer, initState);
+  useEffect(() => {
+    async function fetchHeroes() {
+      try {
+        const heroes = await (await fetch('http://localhost:3001/heroes')).json();
+        await delay(500); // FAKE DELAY
+        dispatch({ type: 'SET_HEROES', payload: heroes });
+      } catch (e) {
+        console.error('Something wrong ...');
+      }
+    }
+    fetchHeroes();
+  }, []);
   const title = 'Tour of Heroes';
   return (
     <Router>
@@ -72,17 +94,27 @@ function App() {
             <Link to="/heroes">Heroes</Link>
           </nav>
           <h1>{title}</h1>
-          <Switch>
-            <Route exact path="/">
-              <Dashboard />
-            </Route>
-            <Route exact path="/heroes">
-              <HeroesList />
-            </Route>
-            <Route exact path="/heroes/:id">
-              <Hero />
-            </Route>
-          </Switch>
+          {state.heroes.length ? (
+            <Switch>
+              <Route exact path="/">
+                <Dashboard />
+              </Route>
+              <Route exact path="/heroes">
+                <HeroesList />
+              </Route>
+              <Route exact path="/heroes/:id">
+                <Hero />
+              </Route>
+            </Switch>
+          ) : (
+            <Loader
+              type="Puff"
+              color="#00BFFF"
+              height={100}
+              width={100}
+              timeout={3000}
+            />
+          )}
         </StateContext.Provider>
       </DispatchContext.Provider>
     </Router>
